002c6f7698d8071c762caba21971abaf5e3e2ecb
AhmedEssamHassan <ahmedessamsakr22@gmail.com>
2020-07-03 04:28:52 +0200

    adding transforming to nav bar

--- script/index.js
+++ script/index.js
@@ -7,22 +7,26 @@ const listItems = document.querySelectorAll("li");
 const imgs = document.querySelectorAll(".nav-img");
 
 for (let x = 0; x < links.length; x++) {
+  titles[x].style.transition = "visibility 3.5s";
   links[x].addEventListener("mouseenter", () => {
-    titles[x].className = "show";
+    listItems[x].style.borderRadius = "35px";
+    titles[x].style.display = "flex";
     imgs[x].style.marginLeft = "15px";
     links[x].style.backgroundColor = "#11111100";
+    links[x].style.color = "white";
     listItems[x].style.backgroundColor = "#ffb400";
-    listItems[x].style.borderRadius = "35px";
   });
 }
 for (let x = 0; x < links.length; x++) {
+  // links[x].style.transition = "all 1s";
   if (x === 0) {
     links[x].addEventListener("mouseleave", () => {
       links[x].style.backgroundColor = "#ffb400";
       listItems[x].style.borderRadius = "50%";
       listItems[x].style.backgroundColor = "#11111100";
       imgs[x].style.marginLeft = "0px";
-      titles[x].className = "";
+      titles[x].style.display = "none";
+      links[x].style.color = "#551a8b";
     });
   } else {
     links[x].addEventListener("mouseleave", () => {
@@ -30,6 +34,8 @@ for (let x = 0; x < links.length; x++) {
       listItems[x].style.backgroundColor = "#11111100";
       imgs[x].style.marginLeft = "0px";
       titles[x].className = "";
+      titles[x].style.display = "none";
+      links[x].style.color = "#551a8b";
     });
   }
 }
