6e261e8772fe4666ec335443bc563ed0083e6503
AhmedEssamHassan <ahmedessamsakr22@gmail.com>
2020-07-04 23:48:02 +0200

    some additional modifications

--- script/index.js
+++ script/index.js
@@ -1,36 +1,47 @@
 const links = document.querySelectorAll(".img");
 
-const titles = document.querySelectorAll(".title");
-
 const listItems = document.querySelectorAll("li");
 
 const imgs = document.querySelectorAll(".nav-img");
 
 for (let x = 0; x < links.length; x++) {
+  const title = document.createElement("h2");
+  switch (x) {
+    case 0:
+      title.textContent = "home";
+      break;
+    case 1:
+      title.textContent = "about";
+      break;
+    case 2:
+      title.textContent = "portfolio";
+      break;
+    case 3:
+      title.textContent = "contact";
+      break;
+    case 4:
+      title.textContent = "blog";
+      break;
+  }
   links[x].addEventListener("mouseenter", () => {
-    titles[x].style.transition = "visibility .75s, opacity .75s ";
-    titles[x].className = "show";
-    titles[x].style.opacity = ".75";
-  });
-}
-
-for (let x = 0; x < links.length; x++) {
-  links[x].addEventListener("mouseleave", () => {
-    titles[x].style.transition = "visibility .75s, opacity .75s ";
-    titles[x].className = "";
-    titles[x].style.opacity = "0";
-    titles[x].style.margin = "0";
+    links[x].appendChild(title);
   });
 }
 
 const btn = document.querySelector(".button");
 const img = document.createElement("img");
-img.style.marginLeft = "7px"
+img.style.marginLeft = "7px";
 img.src = "img/iconfinder_user_1608727.png";
+img.className = "hide";
 btn.addEventListener("mouseenter", () => {
   btn.appendChild(img);
-});
+  img.style.visibility = "visible";
+  img.style.opacity = "1";
+  btn.style.transition = "width 2s";
+  });
 
 btn.addEventListener("mouseleave", () => {
   btn.removeChild(img);
 });
+
+
