58434f4f3d457bc7a8a274c2335b2a056b64b464
AhmedEssamHassan <ahmedessamsakr22@gmail.com>
2020-07-10 03:22:43 +0200

    almost finishing sec section in about page and avtivat color picker on it

--- about/about-script/about.js
+++ about/about-script/about.js
@@ -3,6 +3,8 @@ const colors2 = colorsContainer2.querySelectorAll("li");
 const download = document.querySelector(".download");
 const about = document.querySelector(".about");
 
+const cyrcles = document.querySelectorAll(".cyrcle");
+let z;
 for (let x = 0; x < colors2.length; x++) {
   colors2[x].style.width = "25px";
   colors2[x].style.height = "25px";
@@ -47,7 +49,103 @@ for (let x = 0; x < colors2.length; x++) {
       el.style.color = colors2[x].style.backgroundColor;
     }
 
+    for (let c = 0; c <cyrcles.length; c++) {
+      switch (c) {
+        case 0:
+          cyrcles[c].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) 75%, ${colors2[x].style.backgroundColor} 25%)`;
+          cyrcles[c].style.borderImageSlice = "5";
+          break;
+        case 1:
+          cyrcles[c].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) 30%, ${colors2[x].style.backgroundColor} 25%)`;
+          cyrcles[c].style.borderImageSlice = "5";
+        case 2:
+          cyrcles[c].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) 11%, ${colors2[x].style.backgroundColor} 25%)`;
+          cyrcles[c].style.borderImageSlice = "5";
+        case 3:
+          cyrcles[c].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) 46%, ${colors2[x].style.backgroundColor} 25%)`;
+          cyrcles[c].style.borderImageSlice = "5";
+        case 4:
+          cyrcles[c].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) 34%, ${colors2[x].style.backgroundColor} 25%)`;
+          cyrcles[c].style.borderImageSlice = "5";
+        case 5:
+          cyrcles[c].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) 5%, ${colors2[x].style.backgroundColor} 25%)`;
+          cyrcles[c].style.borderImageSlice = "5";
+        case 6:
+          cyrcles[c].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) 50%, ${colors2[x].style.backgroundColor} 25%)`;
+          cyrcles[c].style.borderImageSlice = "5";
+        case 7:
+          cyrcles[c].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) 35%, ${colors2[x].style.backgroundColor} 25%)`;
+          cyrcles[c].style.borderImageSlice = "5";
+      }
+
+    }
+
     download.style.backgroundColor = colors2[x].style.backgroundColor;
     about.style.backgroundColor = colors2[x].style.backgroundColor;
   });
 }
+
+const siwtchColors = () => {
+  for (let x = 0; x < cyrcles.length; x++) {
+    switch (x) {
+      case 0:
+        z=75;
+        cyrcles[
+          x
+        ].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) ${z}%, rgba(255,179,0,1) 25%)`;
+        cyrcles[x].style.borderImageSlice = "5";
+        break;
+      case 1:
+        z=30;
+        cyrcles[
+          x
+        ].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) ${z}%, rgba(255,179,0,1) 25%)`;
+        cyrcles[x].style.borderImageSlice = "5";
+        break;
+      case 2:
+        z=11;
+        cyrcles[
+          x
+        ].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) ${z}%, rgba(255,179,0,1) 0%)`;
+        cyrcles[x].style.borderImageSlice = "5";
+        break;
+      case 3:
+        z=46;
+        cyrcles[
+          x
+        ].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) ${z}%, rgba(255,179,0,1) 25%)`;
+        cyrcles[x].style.borderImageSlice = "5";
+        break;
+      case 4:
+        z=34;
+        cyrcles[
+          x
+        ].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) ${z}%, rgba(255,179,0,1) 25%)`;
+        cyrcles[x].style.borderImageSlice = "5";
+        break;
+      case 5:
+        z=5;
+        cyrcles[
+          x
+        ].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) ${z}%, rgba(255,179,0,1) 2%)`;
+        cyrcles[x].style.borderImageSlice = "5";
+        break;
+      case 6:
+        z=50;
+        cyrcles[
+          x
+        ].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) ${z}%, rgba(255,179,0,1) 25%)`;
+        cyrcles[x].style.borderImageSlice = "5";
+        break;
+      case 7:
+        z=35;
+        cyrcles[
+          x
+        ].style.borderImage = `linear-gradient(44deg, rgba(34,34,34,1) ${z}%, rgba(255,179,0,1) 5%)`;
+        cyrcles[x].style.borderImageSlice = "5";
+        break;
+    }
+  }
+};
+
+siwtchColors();
